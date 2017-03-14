3e193bc58f1ff978f58a17a8404ff90b04c921c3
RENT <RENT@RENT-Komputer>
2017-03-14 20:34:21 +0100

    new branch?

--- CalculatorGit/src/Main.java
+++ CalculatorGit/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Parser p = new Parser();
-		System.out.println(p.calculateIntFrom("2+2="));
+		System.out.println(p.calculateIntFrom("2+2"));
 
 	}
 
