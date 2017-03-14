9ac5b9e6c0888cc257b2657e1d4664d6bd6b650d
RENT <RENT@RENT-Komputer.home>
2017-03-14 20:43:31 +0100

    Something

--- CalculatorGit/src/Reader.java
+++ CalculatorGit/src/Reader.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 
 public class Reader {
-	
-	public Reader () {
-		Scanner sc= new Scanner(System.in);
-			
+
+	public Reader() {
+	Scanner sc = new Scanner(System.in);
+
 	}
-		
+
 	Scanner sc;
 	boolean readFinished;
 
 	public boolean ReadFinished() {
 		return readFinished;
-	}	
-}
-
+	}
+	
+	private void setReadFinished(boolean readFinished){
+		
+	}
 	
 
+}
