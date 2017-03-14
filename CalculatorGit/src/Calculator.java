139c653cafb0ea822341722e98cbf1d0817c135b
martom87 <marcin.tomala@doctoral.uj.edu.pl>
2017-03-14 20:02:44 +0100

    ...

--- CalculatorGit/src/Calculator.java
+++ CalculatorGit/src/Calculator.java
@@ -1,7 +1,28 @@
 
 public class Calculator {
-	public int calculate(String operation){
-		return 0; 
-		// Test
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
 	}
+
+	@Override
+	public int subtract(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+
+	@Override
+	public int multiply(int a, int b) {
+		// TODO Auto-generated method stub
+		return a*b;
+	}
+
+	@Override
+	public double divide(double a, double b) {
+		if(b==0.0){
+			throw new IllegalArgumentException("nie wolno dzielic przez 0");
+		}
+		return a/b;
 }
+	}
